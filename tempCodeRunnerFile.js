b5a7fe1060a3752d10decea9145fe7131ea794ea
TanishqGhodadra <tanishqghodadra@gmail.com>
2024-04-11 12:38:40 +0530

    3red exam apin in js

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,39 @@
+
+// let products = []
+
+// const uimaker = (data) => {
+
+//     document.getElementById("box").innerHTML = "";
+
+//     data.map((ele, i) => {
+
+
+//         let title = document.createElement("h1")
+//         title.innerHTML = ele.title
+
+
+//         let price = document.createElement("p")
+//         price.innerHTML = ele.price
+
+//         let description = document.createElement("p")
+//         description.innerHTML = ele.description
+
+//         let category = document.createElement("h5")
+//         category.innerHTML = ele.category
+
+//         let image = document.createElement("img")
+//         image.src = ele.image[0];
+
+//         let rating = document.createElement("p")
+//         rating.innerHTML = ele.rating
+
+      
+
+//         let div = document.createElement("div")
+//         div.append(image, title, price, description,category, rating)
+//         div.setAttribute("class", "data");
+//         document.getElementById("box").append(div)
+
+//     })
+
+// }
